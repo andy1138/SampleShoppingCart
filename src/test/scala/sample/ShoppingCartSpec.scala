170a3c3e34ece1694e0da30a290b55729429a99c
Ashwini Rudrappa <ashwini.rudrappa@digital.hmrc.gov.uk>
2018-10-25 16:50:43 +0100

    add test to return total sum of all oranges  and apples in the basket

--- src/test/scala/sample/ShoppingCartSpec.scala
+++ src/test/scala/sample/ShoppingCartSpec.scala
@@ -15,14 +15,19 @@ class ShoppingCartSpec extends FlatSpec with Matchers {
   "ShoppingCart" should "return total price as 0 when supplied empty basket" in {
     val data = List.empty
 
-    ShoppingCart.checkout(data) should be(0)
+    ShoppingCart.checkout(data) should be(0.0)
   }
 
   "Shopping cart" should "return total sum of all oranges in the basket" in {
 
-    ShoppingCart.checkout(List(Orange, Orange, Orange, Orange)) should be(1)
+    ShoppingCart.checkout(List(Orange, Orange, Orange, Orange)) should be(1.0)
 
   }
 
+  "Shopping cart" should "return total sum of all oranges  and apples in the basket" in {
+
+    ShoppingCart.checkout(List(Apple, Orange, Orange, Apple)) should be(1.7)
+
+  }
 
 }
