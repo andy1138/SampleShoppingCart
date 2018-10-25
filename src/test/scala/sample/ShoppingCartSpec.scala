aa6e0c470736fe604896b40dc68eee9d80778549
Ashwini Rudrappa <ashwini.rudrappa@digital.hmrc.gov.uk>
2018-10-25 16:45:48 +0100

    Add test to return the total price as 0 when supplied with empty list

--- src/test/scala/sample/ShoppingCartSpec.scala
+++ src/test/scala/sample/ShoppingCartSpec.scala
@@ -6,10 +6,16 @@ import Constants._
 
 class ShoppingCartSpec extends FlatSpec with Matchers {
 
-  "ShoppingCart" should "return total price of apples in the troly" in {
+  "ShoppingCart" should "return total price of apples in the basket" in {
     val data = List(Apple, Apple, Apple, Apple)
     
     ShoppingCart.checkout(data) should be(2.4)
+  }
+
+  "ShoppingCart" should "return total price as 0 when supplied empty basket" in {
+    val data = List.empty
+
+    ShoppingCart.checkout(data) should be(0)
   }
 
 
